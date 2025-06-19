b5539e89b82a0cdc0bf6111ede0c4f9569368303
Olgu Batın Topal <80317793+Olgu-Batin-Topal@users.noreply.github.com>
2025-06-19 16:03:14 +0300

    Author Controller

---
+++ app/controllers/AuthorController.php
@@ -0,0 +1,78 @@
+<?php
+require_once __DIR__ . '/../models/Authors.php';
+require_once __DIR__ . '/../../helpers/Validator.php';
+
+class AuthorController
+{
+    private $authorModel;
+
+    public function __construct()
+    {
+        $this->authorModel = new Authors();
+    }
+
+    public function index()
+    {
+        $authors = $this->authorModel->all();
+
+        if (empty($authors)) {
+            http_response_code(404);
+            return json_encode(['message' => 'No authors found']);
+        }
+
+        http_response_code(200);
+        return json_encode([
+            'status' => true,
+            'data' => $authors,
+            'message' => 'İşlem başarılı',
+        ]);
+    }
+
+    public function show($id)
+    {
+        $author = $this->authorModel->find($id);
+
+        if (!$author) {
+            http_response_code(404);
+            return json_encode(['message' => 'Author not found']);
+        }
+
+        http_response_code(200);
+        return json_encode($author);
+    }
+
+    public function store($data)
+    {
+        $validator = new Validator($data, [
+            'name' => 'required|max:255',
+            'email' => 'required|max:255|email|unique:authors,email',
+        ]);
+
+        if ($validator->fails()) {
+            http_response_code(422);
+
+            return json_encode([
+                'status' => false,
+                'message' => implode(', ', $validator->errors()[array_key_first($validator->errors())]),
+                'errors' => $validator->errors(),
+            ]);
+        }
+
+        $this->authorModel = new Authors($data);
+        $res = $this->authorModel->create();
+
+        if ($res === true) {
+            http_response_code(201);
+            return json_encode([
+                'status' => true,
+                'message' => 'Author created successfully',
+            ]);
+        } else {
+            http_response_code(500);
+            return json_encode([
+                'status' => false,
+                'message' => $res
+            ]);
+        }
+    }
+}
